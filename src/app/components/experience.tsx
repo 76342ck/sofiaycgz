dc4c97aa24d2adcd162d1ab13a882c8fe972c4cd
76342ck <76342ck@gmail.com>
2024-01-16 18:26:46 -0500

    add vertical timeline

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import React from 'react'
+import SectionHeading from './section-heading'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
 
 export default function Experience() {
-  return (
-    <div>Experience</div>
-  )
+  return <section id='experience'>
+    <SectionHeading>My Experience</SectionHeading>
+    <VerticalTimeline>
+        
+    </VerticalTimeline>
+  </section>
 }
