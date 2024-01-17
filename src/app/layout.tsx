3c3b7f7b721a23d9ce9bad73191e21e5021e7010
76342ck <76342ck@gmail.com>
2024-01-17 15:16:46 -0500

    add dark mode light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from './components/header'
 import ActiveSectionContextProvider from './context/active-section-context'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/footer'
+import ThemeSwitch from './components/theme-switch'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,25 +23,26 @@ export default function RootLayout({
     <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className}
       bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
+
         <div className='bg-[#fbe2e3] absolute top-[-6rem]
           -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] 
-          rounded-full blur-[10rem] sm:w-[68.75rem]'>
+          rounded-full blur-[10rem] sm:w-[68.75rem]
+          dark:bg-[#946263]'>
 
         </div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem]
           left-[-35rem] -z-10 h-[31.25rem] w-[50rem] 
           rounded-full blur-[10rem] sm:w-[68.75rem]
           md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem]
-          2xl:left-[-5rem]'></div>
+          2xl:left-[-5rem] dark:bg-[#676394]'></div>
           
         <ActiveSectionContextProvider>
           <Header />
           {children}
           <Footer />
           <Toaster position='top-right' />
-
         </ActiveSectionContextProvider>
-
+        <ThemeSwitch />
       </body>
     </html>
   )
