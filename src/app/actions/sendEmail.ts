390f0198661a0960533bb32df373fc90d285008d
76342ck <76342ck@gmail.com>
2024-01-16 22:25:42 -0500

    add css

--- src/app/actions/sendEmail.ts
+++ src/app/actions/sendEmail.ts
@@ -2,6 +2,8 @@
 
 import { Resend } from "resend"
 import { validateString, getErrorMessage } from "../lib/utils"
+import ContactFormEmail from "@/email/contact-form-email"
+import React from "react"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -30,7 +32,11 @@ export const sendEmail = async (formData: FormData) => {
             subject: 'Message from Sofia Portfolio',
             reply_to: senderEmail as string,
             // text: message as string,
-            // react: 
+            // react: <ContactFormEmail message={message} senderEmail={sendEmail} />
+            react: React.createElement(ContactFormEmail, {
+                message: message as string,
+                senderEmail: senderEmail as string,
+            })
         })
     } catch (error: unknown) {
         return {
--- src/email/contact-form-email.tsx
+++ src/email/contact-form-email.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import {
+    Html, Body, Head, Heading, Hr, Container, Preview, Section, Text
+} from '@react-email/components'
+import { Tailwind } from '@react-email/tailwind'
 
-export default function ContactFormEmail() {
-  return (
-    <div>ContactFormEmail</div>
-  )
+type ContactFormEmailProps = {
+    message: string;
+    senderEmail: string;
+};
+
+export default function ContactFormEmail({
+    message,
+    senderEmail,
+}: ContactFormEmailProps) {
+    return (
+        <Html>
+            <Head />
+            <Preview>New message from your portfolio site</Preview>
+            <Tailwind>
+                <Body className="bg-gray-100 text-black">
+                    <Container>
+                        <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
+                            <Heading className="leading-tight">
+                                New Message from {senderEmail}
+                            </Heading>
+                            <Hr />
+                            <Text>{message}</Text>
+                        </Section>
+                    </Container>
+                </Body>
+            </Tailwind>
+        </Html>
+    );
 }
