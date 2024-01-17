a9c065c69503815107936feaf856dfa9552cbbcb
76342ck <76342ck@gmail.com>
2024-01-16 22:07:28 -0500

    add handling error message for emails

--- src/app/actions/sendEmail.ts
+++ src/app/actions/sendEmail.ts
@@ -1,12 +1,12 @@
 "use server"
 
 import { Resend } from "resend"
-import { validateString } from "../lib/utils"
+import { validateString, getErrorMessage } from "../lib/utils"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async (formData: FormData) => {
-    const senderEmail = formData.get('email')
+    const senderEmail = formData.get('senderEmail')
     const message = formData.get('message')
 
     // simplified server-side validation for email form
@@ -21,11 +21,19 @@ export const sendEmail = async (formData: FormData) => {
         }
     }
 
-    resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'sofiaycgz@gmail.com',
-        subject: 'Message from Sofia Portfolio',
-        reply_to: senderEmail as string,
-        text: message as string,
-    })
+    new Error('message');
+
+    try {
+        await resend.emails.send({
+            from: 'Contact Form <onboarding@resend.dev>',
+            to: 'sofiaycgz@gmail.com',
+            subject: 'Message from Sofia Portfolio',
+            reply_to: senderEmail as string,
+            text: message as string,
+        })
+    } catch (error: unknown) {
+        return {
+            error: getErrorMessage(error)
+        }
+    }
 }
