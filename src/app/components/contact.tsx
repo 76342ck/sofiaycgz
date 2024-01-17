96e5e1f089da76e7c04038db0df3b275a35442da
76342ck <76342ck@gmail.com>
2024-01-16 22:51:11 -0500

    add css

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -6,15 +6,18 @@ import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { useSectionInView } from '../lib/hooks'
 import { sendEmail } from '../actions/sendEmail'
+import { useFormStatus } from "react-dom"
+import SubmitBtn from './submit-btn'
 
 export default function Contact() {
     const { ref } = useSectionInView("Contact");
+    const { pending } = useFormStatus()
 
     return (
         <motion.section
             ref={ref}
             id='contact'
-            className='mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center'
+            className='mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center text-sm'
             initial={{ opacity: 0, }}
             whileInView={{ opacity: 1, }}
             transition={{ duration: 1, }}
@@ -46,17 +49,7 @@ export default function Contact() {
                     required
                     maxLength={5000}
                 />
-                <button
-                    type='submit'
-                    className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem]
-                  bg-gray-900 text-white rounded-full outline-none transition-all
-                    focus-scale-105 hover:scale-105 hover:bg-gray-950 active:scale-100'>
-                    Submit
-                    <FaPaperPlane
-                        className='text-xs opacity-70 transition-all
-                        group-hover:translate-x-1 group-hover:-translate-y-1'
-                    />{"  "}
-                </button>
+                <SubmitBtn />
             </form>
         </motion.section>
     )
