4562eebcc760bd419d8532d49e8ece0652ab958e
76342ck <76342ck@gmail.com>
2024-01-16 21:47:06 -0500

    add email validation

--- src/app/actions/sendEmail.ts
+++ src/app/actions/sendEmail.ts
@@ -1,7 +1,31 @@
 "use server"
 
+import { Resend } from "resend"
+import { validateString } from "../lib/utils"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
 export const sendEmail = async (formData: FormData) => {
-    console.log('Running on server...')
-    console.log(formData.get("senderEmail"))
-    console.log(formData.get('message'))
+    const senderEmail = formData.get('email')
+    const message = formData.get('message')
+
+    // simplified server-side validation for email form
+    if (!validateString(senderEmail, 500)) {
+        return {
+            error: "Invalid sender email"
+        }
+    }
+    if (!validateString(message, 5000)) {
+        return {
+            error: "Invalid message"
+        }
+    }
+
+    resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'sofiaycgz@gmail.com',
+        subject: 'Message from Sofia Portfolio',
+        reply_to: senderEmail as string,
+        text: message as string,
+    })
 }
