dd2fbf24087352c7c0a0a106ac406a53d9307d70
76342ck <76342ck@gmail.com>
2024-01-16 21:47:17 -0500

    add contact

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -44,7 +44,7 @@ export default function Contact() {
                     name='message'
                     placeholder='Your message...'
                     required
-                    maxLength={500}
+                    maxLength={5000}
                 />
                 <button
                     type='submit'
